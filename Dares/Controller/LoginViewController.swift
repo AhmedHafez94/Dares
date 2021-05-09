76d5dbe6fc4db752eba68f2eb9519f7449c65470
AhmedHafez94 <gm.ahmed.hafez@gmail.com>
2021-05-09 17:19:59 +0200

    Updating Login screen using form field

--- Dares/Controller/LoginViewController.swift
+++ Dares/Controller/LoginViewController.swift
@@ -6,34 +6,20 @@
 //
 
 import UIKit
-import JVFloatLabeledTextField
 
-class LoginViewController: UIViewController, UITextFieldDelegate {
+class LoginViewController: UIViewController {
    
-    @IBOutlet weak var passwordTextField: JVFloatLabeledTextField!
-    @IBOutlet weak var emailTextField: JVFloatLabeledTextField!
+   
     @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var emailStackView: UIStackView!
-    
-    @IBOutlet weak var passwordStackView: UIStackView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         loginButton.roundCorners(radius: 12)
-        emailStackView.roundCorners(radius: 12)
-        passwordStackView.roundCorners(radius: 12)
-        emailTextField.delegate = self
-        passwordTextField.delegate = self
-        
+         
     }
-    
     
     @IBAction func backButtonPressed(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
     }
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
     
-
 }
--- Dares/Controller/SignUpViewController.swift
+++ Dares/Controller/SignUpViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import JVFloatLabeledTextField
 
 class SignUpViewController: UIViewController {
 
